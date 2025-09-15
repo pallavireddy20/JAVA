ce2868592cc58eaafe0ee3fc2f1c2f2fda0954eb
pallavireddy20 <pathikondapallavi9108@gmail.com>
2025-09-15 20:16:04 +0530

    Add files via upload

---
+++ Div.java
@@ -0,0 +1,16 @@
+// import java.util.Scanner;
+// public class Div {
+//     public static void main(String[] args) {
+//     Scanner Sc = new Scanner (System.in);
+//     System.out.println("enter first number");
+//     int a  = Sc.nextInt();
+//     System.out.println("enter second number");
+//     int b = Sc.nextInt();
+//     int remainder = a % b;
+//     System.out.println("remainder = "+ remainder);
+
+
+
+
+//     }
+// }
---
+++ compare.java
@@ -0,0 +1,11 @@
+public class compare {
+    public static void main(String[] args) {
+        int num1 = 15;
+        int num2 = 20;
+        if(num2 >num1){
+            System.out.println("20 is greater");
+        }else{
+            System.out.println("lesser");
+        }
+    }
+}
