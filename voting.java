214d620257b634c11abc6aa8190b0ef88492d8f6
pallavireddy20 <pathikondapallavi9108@gmail.com>
2025-09-15 20:16:39 +0530

    Add files via upload

---
+++ voting.java
@@ -0,0 +1,14 @@
+// import java.util.Scanner;
+// public class voting {
+//     public static void main(String[] args) {
+//         Scanner Sc = new Scanner(System.in);
+//          System.out.println("enter age");
+//          int a = Sc.nextInt();
+//          if(a>=18){
+//             System.out.println("eligible for vote");
+//          }else{
+//             System.out.println("not eligible");
+//          }
+
+//         }
+// }
